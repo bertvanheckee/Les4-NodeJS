37e41e28cf9afb31d1a4ef84d58680e7347165dd
bert.vhofficial@gmail.com <bert.vhofficial@gmail.com>
2022-10-20 10:18:21 +0200

    fix

--- les4/index.js
+++ les4/index.js
@@ -1,48 +1,47 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 const courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'},
-]
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 
-
-app.get('/',(req,res)=>{
-    res.write('<html>');
-    res.write('<body>');
-    res.write('<h1>Hello World</h1>');
-    res.write('</body>');
-    res.write('</html');
-    res.end();
+app.get("/", (req, res) => {
+  res.write("<html>");
+  res.write("<body>");
+  res.write("<h1>Hello World</h1>");
+  res.write("</body>");
+  res.write("</html");
+  res.end();
 });
 
-app.get('/api', (req,res) => {
-    res.send([1,2,3]);
+app.get("/api", (req, res) => {
+  res.send([1, 2, 3]);
 });
 
-
-
-
-app.get('/api/courses/:id', (req,res) => {
-    //res.send(courses);
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send('the course is not found');
-    res.send(course)
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
 });
 
-app.post('/api/courses',(req,res) => {
-    const course = {
-        id: course.length +1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
+app.get("/api/courses/:id", (req, res) => {
+  //res.send(courses);
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("the course is not found");
+  res.send(course);
 });
 
+app.post("/api/courses", (req, res) => {
+  const course = {
+    // courses.length + 1 
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
-
